b1c7e2cd5a8246ffab69062eb80c8c4ae5ae34ce
Efrain Retana <eretana2@hotmail.com>
2020-06-24 19:19:12 -0600

    extend test casese for creating an upload URL, and generating url from file

--- launchpod/src/main/java/com/google/launchpod/servlets/FormHandlerServlet.java
+++ launchpod/src/main/java/com/google/launchpod/servlets/FormHandlerServlet.java
@@ -1,12 +1,15 @@
 package com.google.launchpod.servlets;
 
-import com.google.appengine.api.blobstore.BlobstoreService;
-import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
 import com.google.appengine.api.blobstore.BlobInfo;
 import com.google.appengine.api.blobstore.BlobInfoFactory;
 import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+import com.google.appengine.api.images.ServingUrlOptions;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 import java.util.Map;
 
@@ -29,6 +32,20 @@ public class FormHandlerServlet extends HttpServlet {
     BlobstoreService blobstore = BlobstoreServiceFactory.getBlobstoreService();
     Map<String, List<BlobKey>> blobs = blobstore.getUploads(req);
     List<BlobKey> keys = blobs.get(inputName);
+
+    //When there is nothing uploaded
+    if(keys.isEmpty() || keys == null){
+      return null;
+    }
+    //Get first input from form
+    BlobKey key = keys.get(0);
+
+    //when user does not select form
+    BlobInfo blobInfo = new BlobInfoFactory().loadBlobInfo(key);
+    if(blobInfo.getSize() == 0){
+      blobstore.delete(key);
+      return null;
+    }
 
     return "";
   }
