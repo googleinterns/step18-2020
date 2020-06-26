5ee856dc0cd67b07830e8cb60e45fad207c22802
eretana <eretana@google.com>
2020-06-25 19:07:42 -0600

    Finish xml String, add property to userFeedEntity

--- launchpod/src/main/java/com/google/launchpod/data/UserFeed.java
+++ launchpod/src/main/java/com/google/launchpod/data/UserFeed.java
@@ -7,10 +7,12 @@ public final class UserFeed{
   private static final String name;
   private static final String mp3Link;
   private static final long timestamp;
+  private static final String xmlString;
 
-  public UserFeed(String name, String mp3Link){
+  public UserFeed(String name, String mp3Link, long timestamp, String xmlString){
     this.name = name; 
     this.mp3Link = mp3Link;
     this.timestamp = timestamp;
+    this.xmlString = xmlString;
   }
 }
--- launchpod/src/main/java/com/google/launchpod/servlets/FormHandlerServlet.java
+++ launchpod/src/main/java/com/google/launchpod/servlets/FormHandlerServlet.java
@@ -20,33 +20,54 @@ import javax.servlet.http.HttpServletResponse;
 public class FormHandlerServlet extends HttpServlet {
   
   private static final String USER_FEED = "UserFeed";
-  private static final String PODCAST_NAME = "name";
+  private static final String PODCAST_TITLE = "title";
   private static final String TIMESTAMP = "timestamp";
   private static final String MP3LINK = "mp3link";
-
-  private static final String 
+  private static final String XML_STRING = "xmlString";
 
   /**
    *  request user inputs in form fields then create Entity and place in datastore
    */
   @Override
   public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException {
-    String name = req.getParameter(NAME);
+    String podcastTitle = req.getParameter(PODCAST_TITLE);
     String mp3Link = req.getParameter(MP3LINK);
     long timestamp = System.currentTimeMillis();
+    String xmlString - xmlString(name, mp3Link);
 
     Entity userFeedEntity = new Entity(USER_FEED);
-    userFeedEntity.setProperty(NAME, name);
+    userFeedEntity.setProperty(PODCAST_TITLE, podcastTitle);
     userFeedEntity.setProperty(MP3LINK, mp3Link);
     userFeedEntity.setProperty(TIMESTAMP, timestamp);
+    userFeedEntity.setProperty(XML_STRING ,xmlString);
 
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     datastore.put(userFeedEntity);
   }
 
-  private String xmlString(String name, String mp3L){
-    String xmlString = "";
-    return xmlString
+  private String xmlString(String name, String mp3Link){
+    String xmlString = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"  +
+                       "<rss version=\"2.0\">" + 
+                       "  <channel>" +
+                       "    <language>en</language>"  +
+                       "    <itunes:author>User</itunes:author>" + 
+                       "    <title>" + name + "</title>" + 
+                       "    <item>" +
+                       "      <title>" + name + "</title>" + 
+                       "      <summary>This is episode 4</summary>" + 
+                       "      <description>This is episode 4</description>" + 
+                       "      <link>" + mp3Link +"</link>" +
+                       "      <enclosure url=\"" + mp3Link + "\" type=\"audio/mpeg\" length=\"185000\"/>" +
+                       "      <pubDate>Thu, 20 Apr 2020 04:20:00 +0800</pubDate>" +
+                       "      <itunes:author/>" + 
+                       "      <itunes:duration>03:05</itunes:duration>" + 
+                       "      <itunes:explicit>No</itunes:explicit>" + 
+                       "      <guid isPermaLink=\"false\">uhwefpoihEOUUHSFEOIwqkhdho-=</guid>" + 
+                       "    </item>"
+                       "  </channel>"
+                       "</rss>";
+
+    return xmlString;
 
   }
 
