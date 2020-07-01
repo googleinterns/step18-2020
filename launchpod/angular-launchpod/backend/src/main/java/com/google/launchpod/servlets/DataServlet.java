771262afe5d442635fea73a07137e1742c87ca86
Jag <jagarijennings@gmail.com>
2020-07-01 16:58:13 +0000

    Edit outputPath and restructure files.

--- launchpod/angular-launchpod/backend/src/main/java/com/google/launchpod/servlets/DataServlet.java
+++ launchpod/angular-launchpod/backend/src/main/java/com/google/launchpod/servlets/DataServlet.java
@@ -26,6 +26,6 @@ public class DataServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello world!</h1>");
+    response.getWriter().println("launchpod-step18-2020.appspot.com");
   }
 }
--- launchpod/angular-launchpod/src/app/form-handler.service.ts
+++ launchpod/angular-launchpod/src/app/form-handler.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
+import {map} from 'rxjs/operators';
 
 const FEED_URL = '/load-result';
 
@@ -14,7 +15,7 @@ export class FormHandlerService {
   /**
    * Fetch feed url from back-end
    */
-  getFeedUrl(): Observable<string> {
-    return this.http.get<string>(FEED_URL);
+  getFeedUrl(): Observable<string>{
+    return this.http.get(FEED_URL,{ responseType: 'text' });
   }
 }
