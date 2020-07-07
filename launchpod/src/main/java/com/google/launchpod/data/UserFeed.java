c39bb8fccb863fc6b87023497c53d47d94226e60
Efrain Retana <eretana2@hotmail.com>
2020-07-07 11:02:13 -0600

    fix minor bug issue with unecessary pubdate inside of userfeed class

--- launchpod/src/main/java/com/google/launchpod/data/UserFeed.java
+++ launchpod/src/main/java/com/google/launchpod/data/UserFeed.java
@@ -7,12 +7,10 @@ public final class UserFeed{
 
   private final String podcastTitle;
   private final String mp3Link;
-  private final String pubDate;
 
-  private UserFeed(String podcastTitle, String mp3Link, String pubDate) {
+  private UserFeed(String podcastTitle, String mp3Link) {
     this.podcastTitle = podcastTitle;
     this.mp3Link = mp3Link;
-    this.pubDate = pubDate;
   }
   /** Create UserFeed Object from Entity
    *  @param entity : entity that is being used to create user feed object
@@ -21,9 +19,8 @@ public final class UserFeed{
   public static UserFeed fromEntity(Entity entity){
     String podcastTitle = (String) entity.getProperty(FormHandlerServlet.PODCAST_TITLE);
     String mp3Link = (String) entity.getProperty(FormHandlerServlet.MP3LINK);
-    String pubDate = (String) entity.getProperty(FormHandlerServlet.PUB_DATE);
 
-    return new UserFeed(podcastTitle, mp3Link, pubDate);
+    return new UserFeed(podcastTitle, mp3Link);
   }
 
   public String getTitle(){
@@ -32,9 +29,5 @@ public final class UserFeed{
 
   public String getLink(){
     return this.mp3Link;
-  }
-
-  public String getPubDate(){
-    return this.pubDate;
   }
 }
