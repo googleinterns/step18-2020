cf79fec1ff617f57eb06a2f2a1488f7267ccd5c3
Efrain Retana <eretana2@hotmail.com>
2020-07-10 13:47:09 -0600

    Add async method to recognize mp3 from cloud

--- launchpod/angular-launchpod/backend/src/main/java/com/google/launchpod/servlets/TranscriptionServet.java
+++ launchpod/angular-launchpod/backend/src/main/java/com/google/launchpod/servlets/TranscriptionServet.java
@@ -8,7 +8,47 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/transcribe-feed")
 public class TranscriptionServlet extends HttpServlet {
 
-  public void doPost(HttpServletRequest req, HttpServletResponse res) {
-      
+  public void doPost(HttpServletRequest req, HttpServletResponse res) {}
+
+  /**
+   * Performs non-blocking speech recognition on remote FLAC file and prints the transcription.
+   * @param gcsUri the path to remote file to be transcribed
+   */
+  public static void asyncRecognizeGcs(String gcsUri) throws Exception {
+      //create client credentials
+    try (SpeechClient speech = SpeechClient.create()) {
+      // Configure remote file request for FLAC
+      RecognitionConfig config = RecognitionConfig
+        .newBuilder()
+        .setEncoding(AudioEncoding.FLAC)
+        .setLanguageCode("en-US")
+        .setSampleRateHertz(16000)
+        .build();
+      RecognitionAudio audio = RecognitionAudio
+        .newBuilder()
+        .setUri(gcsUri)
+        .build();
+
+      // Use non-blocking call for getting file transcription
+      OperationFuture<LongRunningRecognizeResponse, LongRunningRecognizeMetadata> response = speech.longRunningRecognizeAsync(
+        config,
+        audio
+      );
+      while (!response.isDone()) {
+        System.out.println("Waiting for response...");
+        Thread.sleep(10000);
+      }
+
+      List<SpeechRecognitionResult> results = response.get().getResultsList();
+
+      for (SpeechRecognitionResult result : results) {
+        // There can be several alternative transcripts for a given chunk of speech. Just use the
+        // first (most likely) one here.
+        SpeechRecognitionAlternative alternative = result
+          .getAlternativesList()
+          .get(0);
+        System.out.printf("Transcription: %s\n", alternative.getTranscript());
+      }
+    }
   }
 }
