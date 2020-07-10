52871c68cbe06f29e5dcb7f54a7ccc3f23a0f3d9
Efrain Retana <eretana2@hotmail.com>
2020-07-10 13:33:42 -0600

    create servlet for transcription

---
+++ launchpod/angular-launchpod/backend/src/main/java/com/google/launchpod/servlets/TranscriptionServet.java
@@ -0,0 +1,14 @@
+package com.google.launchpod.servlets;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/transcribe-feed")
+public class TranscriptionServlet extends HttpServlet {
+
+  public void doPost(HttpServletRequest req, HttpServletResponse res) {
+      
+  }
+}
