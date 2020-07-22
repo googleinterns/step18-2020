831363e8e72a85a857135bd74780a9a57294d1e8
Efrain Retana <eretana2@hotmail.com>
2020-07-22 03:43:20 +0000

    Fix bug to allow deserialization from xml string to object. Adjust test cases to better test creation of data

--- launchpod/angular-launchpod/backend/src/main/java/com/google/launchpod/servlets/TranslationServlet.java
+++ launchpod/angular-launchpod/backend/src/main/java/com/google/launchpod/servlets/TranslationServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
@@ -47,65 +48,58 @@ public class TranslationServlet extends HttpServlet {
     if (link == null) {
       throw new IOException("Please give valid link.");
     }
+    
     if (targetLanguage == null) {
       throw new IOException("Please give valid language.");
     }
     try {
+      XML_MAPPER.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
       String id = getIdFromUrl(link);
 
       Key desiredFeedKey = KeyFactory.stringToKey(id);
       DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-      try {
-        Entity desiredFeedEntity = datastore.get(desiredFeedKey);
-        String xmlString = (String) desiredFeedEntity.getProperty(XML_STRING);
-        RSS rssFeed = XML_MAPPER.readValue(xmlString, RSS.class);
-
-        // Translate fields to new language
-        Translate translate = TranslateOptions.getDefaultInstance().getService();
-
-        // Channel description
-        Translation translation = translate.translate(rssFeed.getChannel().getDescription());
-        rssFeed.getChannel().setDescription(translation.getTranslatedText());
-
-        // Language
-        rssFeed.getChannel().setLanguage(targetLanguage);
-
-        // Episodes
-        for (Item item : rssFeed.getChannel().getItem()) {
-          // Episode title
-          translation = translate.translate(item.getTitle());
-          item.setTitle(translation.getTranslatedText());
-
-          // Episode description
-          translation = translate.translate(item.getDescription());
-          item.setDescription(translation.getTranslatedText());
-        }
-
-        // Generate Translated XML string then place it into datastore
-        String translatedXmlString = RSS.toXmlString(rssFeed);
-        Entity translatedUserFeedEntity = new Entity(USER_FEED);
-        translatedUserFeedEntity.setProperty(XML_STRING, translatedXmlString);
-        String translatedFeedId = KeyFactory.keyToString(datastore.put(translatedUserFeedEntity));
-
-        // display new translated string to the user
-        res.setContentType("text/html");
-        res.getWriter().println(BASE_URL + translatedFeedId);
-
-      } catch (EntityNotFoundException e) {
-        res.setContentType("text/html");
-        res.sendError(HttpServletResponse.SC_NOT_FOUND, "Requested ID was not found.");
-        return;
+      Entity desiredFeedEntity = datastore.get(desiredFeedKey);
+      if(desiredFeedEntity == null){
+          throw new EntityNotFoundException(desiredFeedKey);
       }
-    } catch (Exception e) {
-      res.sendError(HttpServletResponse.SC_CONFLICT, "Unable to obtain ID from given link");
-      return;
-    }
-    /*
-     * String projectId = "launchpod-step18-2020"; // Supported Languages:
-     * https://cloud.google.com/translate/docs/languages String text =
-     * "transcribed feed here"; translateText(projectId, targetLanguage, text);
-     */
+      String xmlString = (String) desiredFeedEntity.getProperty(XML_STRING);
+      RSS rssFeed = XML_MAPPER.readValue(xmlString, RSS.class);
 
+      // Translate fields to new language
+      Translate translate = TranslateOptions.getDefaultInstance().getService();
+
+      // Channel description
+      Translation translation = translate.translate(rssFeed.getChannel().getDescription());
+      rssFeed.getChannel().setDescription(translation.getTranslatedText());
+
+      // Language
+      rssFeed.getChannel().setLanguage(targetLanguage);
+
+      // Episodes
+      for (Item item : rssFeed.getChannel().getItem()) {
+        // Episode title
+        translation = translate.translate(item.getTitle());
+        item.setTitle(translation.getTranslatedText());
+
+        // Episode description
+        translation = translate.translate(item.getDescription());
+        item.setDescription(translation.getTranslatedText());
+      }
+
+      // Generate Translated XML string then place it into datastore
+      String translatedXmlString = RSS.toXmlString(rssFeed);
+      Entity translatedUserFeedEntity = new Entity(USER_FEED);
+      translatedUserFeedEntity.setProperty(XML_STRING, translatedXmlString);
+      Key newFeedKey = datastore.put(translatedUserFeedEntity);
+      String translatedFeedId = KeyFactory.keyToString(translatedUserFeedEntity.getKey());
+
+      // display new translated string to the user
+      res.setContentType("text/html");
+      res.getWriter().println(BASE_URL + translatedFeedId);
+      } catch (EntityNotFoundException e) {
+        res.sendError(HttpServletResponse.SC_CONFLICT, "Unable to obtain ID from given link");
+        return;
+    }
   }
 
   /**
